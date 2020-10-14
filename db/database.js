1dfced1288c1cfbe7ee701dcff136bc60b9b8bab
WalaNour <nourwala548@gmail.com>
2020-10-14 19:41:52 +0100

    fixed

--- db/database.js
+++ db/database.js
@@ -10,17 +10,16 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-let registere = (arr,  callback) => {
-  console.log("hheheh")
-  var sql = `UPDATE student  SET(firstname, lastname, country, city, addresspostal , driving , dateOfBirth , placeOfBirth , nationality , education , socialLink , skills , languages , hobbies , profilePic , summery) values (?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ?)  WHERE username= ?;`;
+let registere = (arr, callback) => {
+  console.log("hheheh");
+  console.log(arr)
+  var sql = `UPDATE student SET firstname = ? , lastname = ? , country = ?, city = ?, addresspostal = ?, driving = ?, dateOfBirth = ?, placeOfBirth = ?, nationality = ?, education = ?, socialLink = ?, skills = ?, languages = ?, hobbies = ?, profilePic = ?, summery= ? WHERE username= ?;`;
   connection.query(sql, arr, (err, data) => {
     if (err) throw callback(err);
     callback(null, data);
   });
 };
 
-
-
 module.exports = {
-  registere , 
+  registere,
 };
